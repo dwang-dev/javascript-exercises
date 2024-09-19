9f0a999655f89dac7ca57d4088a3fbad472d68d1
Daniel <142920963+dwang-dev@users.noreply.github.com>
2024-09-19 17:33:06 +1000

    Finished exercises 8-12.
    These exercises practiced working with javascript arrays and objects.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr) {
+    let oldest_age = -1;
+    let oldest_person = {}
+    for (let person of peopleArr) {
+        let age = 0;
+        if (person.yearOfDeath === undefined) {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+        if (age > oldest_age) {
+            oldest_person = person;
+            oldest_age = age;
+        }
+    }
+    return oldest_person;
 };
 
 // Do not edit below this line
